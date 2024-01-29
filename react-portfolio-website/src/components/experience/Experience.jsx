4a77dc632d2974120a6294e87d1cc001657be8a1
Stephen Brockbank <90523707+swbrock@users.noreply.github.com>
2024-01-29 12:50:01 -0700

    clean up experience

--- react-portfolio-website/src/components/experience/Experience.jsx
+++ react-portfolio-website/src/components/experience/Experience.jsx
@@ -105,20 +105,7 @@ const Experience = () => {
                 <small className='text-light'>Experienced</small>
               </div>
             </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>Node JS</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
-            <article className='experience__details'>
-              <BsPatchCheckFill className='experience__details-icon' />
-              <div>
-                <h4>MongoDB</h4>
-                <small className='text-light'>Intermediate</small>
-              </div>
-            </article>
+            
           </div>
           <h3>Technologies</h3>
           <div className="experience__content">
