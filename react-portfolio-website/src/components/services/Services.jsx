d8742ce089867f7d163b0a1404c84f95975096f6
Stephen Brockbank <90523707+swbrock@users.noreply.github.com>
2024-01-30 10:49:13 -0700

    update education

--- react-portfolio-website/src/components/services/Services.jsx
+++ react-portfolio-website/src/components/services/Services.jsx
@@ -9,33 +9,36 @@ const services = () => {
             <h2>Education</h2>
 
             <div className="container services__container">
-                <article className="service">
-                    <div className="service__head">
-                        <h3>Cybersecurity</h3>
+                <article className='service'>
+                    <div className='service__head'>
+                        <h3>Statistics</h3>
                     </div>
 
                     <ul className='service__list'>
                         <li>
                             <BiCheck className='service__list-icon' />
-                            <p>Information Security Principles</p>
+                            <p>Data Analytics and Machine Learning using Azure ML Studio</p>
                         </li>
                         <li>
                             <BiCheck className='service__list-icon' />
-                            <p>Identify Information Security Threats and Vulnerabilities</p>
+                            <p>Data wrangling and data cleaning in R</p>
                         </li>
                         <li>
                             <BiCheck className='service__list-icon' />
-                            <p>Attack, Prevention, and Detection</p>
+                            <p>Machine Learning in Python</p>
                         </li>
                         <li>
                             <BiCheck className='service__list-icon' />
-                            <p>Internal Controls of Business Processes</p>
+                            <p>Compute summary statistics from R and SAS datasets</p>
                         </li>
                         <li>
                             <BiCheck className='service__list-icon' />
-                            <p>Project Management using SCRUM methodology</p>
+                            <p>Writing Statistical Models</p>
                         </li>
-
+                        <li>
+                            <BiCheck className='service__list-icon' />
+                            <p>Understand Experimental Design</p>
+                        </li>
                     </ul>
                 </article>
 
@@ -55,10 +58,6 @@ const services = () => {
                         </li>
                         <li>
                             <BiCheck className='service__list-icon' />
-                            <p>Web Development with Python using Django and PostGreSQL</p>
-                        </li>
-                        <li>
-                            <BiCheck className='service__list-icon' />
                             <p>Web Development with JavaScript using Node/Express and PostGreSQL</p>
                         </li>
                         <li>
@@ -68,33 +67,35 @@ const services = () => {
                     </ul>
                 </article>
 
+
                 <article className='service'>
                     <div className='service__head'>
-                        <h3>Statistics</h3>
+                        <h3>Computer Science</h3>
                     </div>
 
                     <ul className='service__list'>
                         <li>
                             <BiCheck className='service__list-icon' />
-                            <p>Data Analytics and Machine Learning using Azure ML Studio</p>
+                            <p>Create phone and tablet apps using Swift and SwiftUI</p>
                         </li>
                         <li>
                             <BiCheck className='service__list-icon' />
-                            <p>Machine Learning in Python</p>
+                            <p>Built a lexer in Python</p>
                         </li>
                         <li>
                             <BiCheck className='service__list-icon' />
-                            <p>Compute summary statistics from R and SAS datasets</p>
+                            <p>Built the game of chess in Java</p>
                         </li>
                         <li>
                             <BiCheck className='service__list-icon' />
-                            <p>Writing Statistical Models</p>
+                            <p>Voice Interface with Amazon services using Alexa skills kit with python</p>
                         </li>
                         <li>
                             <BiCheck className='service__list-icon' />
-                            <p>Understand Experimental Design</p>
+                            <p>GPA in all of my computer sciences classes is a 3.8</p>
                         </li>
                     </ul>
+
                 </article>
 
             </div>
