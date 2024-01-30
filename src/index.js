8d121ce57625319ee8f457469f2d5e194e7468eb
Stephen Brockbank <90523707+swbrock@users.noreply.github.com>
2024-01-30 11:29:46 -0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
-ReactDOM.render(<App/>, document.querySelector('#root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
+reportWebVitals();
