e64836e444c64ee2e1906f2d1135ba446348f13a
Stephen Brockbank <90523707+swbrock@users.noreply.github.com>
2024-01-31 16:28:30 -0700

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,10 +12,8 @@ const Footer = () => {
       <ul className='permalinks'>
         <li><a href='#'>Home</a></li>
         <li><a href='#about'>About</a></li>
-        <li><a href='#experiences'>Experiences</a></li>
+        <li><a href='#experience'>Experiences</a></li>
         <li><a href='#education'>Education</a></li>
-        {/* <li><a href='#porfolio'>Portfolio</a></li> */}
-        {/* <li><a href='#testimonials'>Testimonials</a></li> */}
         <li><a href='#contact'>Contact</a></li>
       </ul>
 
